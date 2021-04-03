7f3d613834911b4e7ec7b4832eba4b2e37da8b88
sergfedoro@mail.ru <sergfedoro@mail.ru>
2021-04-03 20:30:38 +0300

    Implement an HTTPClient to access Web API from codebehind

---
+++ WebApp/HTTPClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApp
+{
+    public sealed class HTTPClient : HttpClient
+    {
+        private HTTPClient() { }
+
+        private static HTTPClient _Instance = new HTTPClient();
+
+        public static HTTPClient Instance
+        {
+            get
+            {
+                if (_Instance.BaseAddress == null)
+                {
+                    _Instance.BaseAddress =
+                        new Uri(HttpContext.Current.Request.Url.Scheme + "://"
+                        + HttpContext.Current.Request.Url.Authority);
+                }
+                return _Instance;
+            }
+        }
+    }
+}
